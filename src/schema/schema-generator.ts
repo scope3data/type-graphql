34685f571ed0e9e63f521858dcd51c63f35cd08f
Ben Miner <bminer@scope3.com>
2025-04-18 11:29:44 -0500

    implement maps in type info in schema generator

--- src/schema/schema-generator.ts
+++ src/schema/schema-generator.ts
@@ -109,15 +109,21 @@ export type SchemaGeneratorOptions = {
 export abstract class SchemaGenerator {
   private static objectTypesInfo: ObjectTypeInfo[] = [];
 
-  // private static objectTypesInfoMap = new Map<Function, ObjectTypeInfo>();
+  private static objectTypesInfoMap = new Map<Function, ObjectTypeInfo>();
 
   private static inputTypesInfo: InputObjectTypeInfo[] = [];
 
+  private static inputTypesInfoMap = new Map<Function, InputObjectTypeInfo>();
+
   private static interfaceTypesInfo: InterfaceTypeInfo[] = [];
+
+  private static interfaceTypesInfoMap = new Map<Function, InterfaceTypeInfo>();
 
   private static enumTypesInfo: EnumTypeInfo[] = [];
 
   private static unionTypesInfo: UnionTypeInfo[] = [];
+
+  private static unionTypesInfoMap = new Map<symbol, UnionTypeInfo>();
 
   private static usedInterfaceTypes = new Set<Function>();
 
@@ -203,13 +209,11 @@ export abstract class SchemaGenerator {
         unionObjectTypesInfo.push(
           ...unionMetadata
             .getClassTypes()
-            .map(
-              objectTypeCls => this.objectTypesInfo.find(type => type.target === objectTypeCls)!,
-            ),
+            .map(objectTypeCls => this.objectTypesInfoMap.get(objectTypeCls)!),
         );
         return unionObjectTypesInfo.map(it => it.type);
       };
-      return {
+      const unionType = {
         unionSymbol: unionMetadata.symbol,
         type: new GraphQLUnionType({
           name: unionMetadata.name,
@@ -236,7 +240,12 @@ export abstract class SchemaGenerator {
               },
         }),
       };
+
+      this.unionTypesInfoMap.set(unionMetadata.symbol, unionType);
+
+      return unionType;
     });
+
     this.enumTypesInfo = this.metadataStorage.enums.map<EnumTypeInfo>(enumMetadata => {
       const enumMap = getEnumValuesMap(enumMetadata.enumObj);
       return {
@@ -257,17 +266,18 @@ export abstract class SchemaGenerator {
         }),
       };
     });
+
     this.objectTypesInfo = this.metadataStorage.objectTypes.map<ObjectTypeInfo>(objectType => {
       const objectSuperClass = Object.getPrototypeOf(objectType.target);
       const hasExtended = objectSuperClass.prototype !== undefined;
       const getSuperClassType = () => {
         const superClassTypeInfo =
-          this.objectTypesInfo.find(type => type.target === objectSuperClass) ??
-          this.interfaceTypesInfo.find(type => type.target === objectSuperClass);
+          this.objectTypesInfoMap.get(objectSuperClass) ??
+          this.interfaceTypesInfoMap.get(objectSuperClass);
         return superClassTypeInfo ? superClassTypeInfo.type : undefined;
       };
       const interfaceClasses = objectType.interfaceClasses || [];
-      return {
+      const objectTypeInfo = {
         metadata: objectType,
         target: objectType.target,
         type: new GraphQLObjectType({
@@ -277,9 +287,7 @@ export abstract class SchemaGenerator {
           extensions: objectType.extensions,
           interfaces: () => {
             let interfaces = interfaceClasses.map<GraphQLInterfaceType>(interfaceClass => {
-              const interfaceTypeInfo = this.interfaceTypesInfo.find(
-                info => info.target === interfaceClass,
-              );
+              const interfaceTypeInfo = this.interfaceTypesInfoMap.get(interfaceClass);
               if (!interfaceTypeInfo) {
                 throw new Error(
                   `Cannot find interface type metadata for class '${interfaceClass.name}' ` +
@@ -371,6 +379,8 @@ export abstract class SchemaGenerator {
           },
         }),
       };
+      this.objectTypesInfoMap.set(objectType.target, objectTypeInfo);
+      return objectTypeInfo;
     });
     this.interfaceTypesInfo = this.metadataStorage.interfaceTypes.map<InterfaceTypeInfo>(
       interfaceType => {
@@ -395,7 +405,7 @@ export abstract class SchemaGenerator {
           implementingObjectTypesTargets.includes(objectTypesInfo.target),
         );
 
-        return {
+        const interfaceTypeInfo = {
           metadata: interfaceType,
           target: interfaceType.target,
           type: new GraphQLInterfaceType({
@@ -404,8 +414,7 @@ export abstract class SchemaGenerator {
             astNode: getInterfaceTypeDefinitionNode(interfaceType.name, interfaceType.directives),
             interfaces: () => {
               let interfaces = (interfaceType.interfaceClasses || []).map<GraphQLInterfaceType>(
-                interfaceClass =>
-                  this.interfaceTypesInfo.find(info => info.target === interfaceClass)!.type,
+                interfaceClass => this.interfaceTypesInfoMap.get(interfaceClass)!.type,
               );
               // copy interfaces from super class
               if (hasExtended) {
@@ -490,18 +499,18 @@ export abstract class SchemaGenerator {
                 },
           }),
         };
+        this.interfaceTypesInfoMap.set(interfaceType.target, interfaceTypeInfo);
+        return interfaceTypeInfo;
       },
     );
     this.inputTypesInfo = this.metadataStorage.inputTypes.map<InputObjectTypeInfo>(inputType => {
       const objectSuperClass = Object.getPrototypeOf(inputType.target);
       const getSuperClassType = () => {
-        const superClassTypeInfo = this.inputTypesInfo.find(
-          type => type.target === objectSuperClass,
-        );
+        const superClassTypeInfo = this.inputTypesInfoMap.get(objectSuperClass);
         return superClassTypeInfo ? superClassTypeInfo.type : undefined;
       };
       const inputInstance = new (inputType.target as any)();
-      return {
+      const inputTypeInfo = {
         target: inputType.target,
         type: new GraphQLInputObjectType({
           name: inputType.name,
@@ -547,6 +556,8 @@ export abstract class SchemaGenerator {
           astNode: getInputObjectTypeDefinitionNode(inputType.name, inputType.directives),
         }),
       };
+      this.inputTypesInfoMap.set(inputType.target, inputTypeInfo);
+      return inputTypeInfo;
     });
   }
 
