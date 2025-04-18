679067b067fbaaca85897d7d99d432291615871d
Ben Miner <bminer@scope3.com>
2025-04-18 10:39:32 -0500

    revert src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 export * from "./decorators";
 export * from "./errors";
-export { SchemaGenerator } from "./schema/schema-generator";
-export { MetadataStorage } from "./metadata/metadata-storage";
 export * from "./typings";
 export * from "./metadata";
 export * from "./scalars";
